0a012c3d594541df31e00827c82dca712603bce2
Cameron Varley <cam.avarley@gmail.com>
2021-12-09 18:37:53 -0500

    First Commit

---
+++ lineage_timelm.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# TODO: Add file
+# Inherit from instantnoodlep device
+# $(call inherit-product, device/oneplus/instantnoodlep/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_timelm
+PRODUCT_DEVICE := timelm
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LM-V600
+
+# TODO: What is this
+# PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+# TODO: Update fingerprint
+# PRODUCT_BUILD_PROP_OVERRIDES += \
+#     PRODUCT_DEVICE=timelm \
+#     PRODUCT_NAME=timelm \
+#     PRIVATE_BUILD_DESC="OnePlus8Pro-user 11   release-keys"
+
+BUILD_FINGERPRINT := lge/timelm/timelm:11/RKQ1.201105.002/2120114401c44:user/release-keys
